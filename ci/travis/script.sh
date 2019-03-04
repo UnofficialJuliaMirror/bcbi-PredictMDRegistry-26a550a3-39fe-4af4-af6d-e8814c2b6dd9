7b7d5d455f246d0baf4b3c8a8d22319be8d34d3f
Dilum Aluthge <dilum@aluthge.com>
2019-03-04 18:11:58 -0500

    Progress commit

---
+++ ci/travis/script.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+##### Beginning of file
+
+set -ev
+
+export TRAVIS_JULIA_VERSION=$JULIA_VER
+echo "TRAVIS_JULIA_VERSION=$TRAVIS_JULIA_VERSION"
+
+export JULIA_FLAGS="--check-bounds=yes --code-coverage=all --color=yes --compiled-modules=yes --inline=no"
+echo "JULIA_FLAGS=$JULIA_FLAGS"
+
+julia $JULIA_FLAGS -e "VERSION >= v\"0.7.0-DEV.3630\" && using InteractiveUtils; versioninfo()"
+
+julia $JULIA_FLAGS -e '
+    TRAVIS_BUILD_DIR = strip(ENV["TRAVIS_BUILD_DIR"]);
+    @info("TRAVIS_BUILD_DIR: ", TRAVIS_BUILD_DIR,)
+    import Pkg;
+    Pkg.Registry.add(Pkg.RegistrySpec(path=TRAVIS_BUILD_DIR));
+    '
+
+julia $JULIA_FLAGS -e '
+    import Pkg;
+    Pkg.add("PredictMD");
+    Pkg.build("PredictMD");
+    '
+
+julia $JULIA_FLAGS -e '
+    import Pkg;
+    Pkg.add("PredictMDExtra");
+    Pkg.build("PredictMDExtra");
+    '
+
+# julia $JULIA_FLAGS -e '
+#     import Pkg;
+#     Pkg.add("PredictMDFull");
+#     Pkg.build("PredictMDFull");
+#     '
+
+julia $JULIA_FLAGS -e '
+    import Pkg;
+    Pkg.build("PredictMD");
+    Pkg.build("PredictMDExtra");
+    '
+# julia $JULIA_FLAGS -e '
+#     import Pkg;
+#     Pkg.build("PredictMD");
+#     Pkg.build("PredictMDExtra");
+#     Pkg.build("PredictMDFull");
+#     '
+
+##### End of file
