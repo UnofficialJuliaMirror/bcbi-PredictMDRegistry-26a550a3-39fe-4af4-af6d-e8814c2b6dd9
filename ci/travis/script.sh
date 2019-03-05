99dac0c35e4e7905fa45709aad0b829d17eabc49
Dilum Aluthge <dilum@aluthge.com>
2019-03-04 19:51:53 -0500

    Progress commit

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -13,6 +13,7 @@ echo "JULIA_FLAGS=$JULIA_FLAGS"
 julia $JULIA_FLAGS -e "VERSION >= v\"0.7.0-DEV.3630\" && using InteractiveUtils; versioninfo()"
 
 julia $JULIA_FLAGS -e '
+    ENV["JULIA_DEBUG"] = "all";
     TRAVIS_BUILD_DIR = strip(ENV["TRAVIS_BUILD_DIR"]);
     @info("TRAVIS_BUILD_DIR: ", TRAVIS_BUILD_DIR,)
     import Pkg;
@@ -20,33 +21,32 @@ julia $JULIA_FLAGS -e '
     '
 
 julia $JULIA_FLAGS -e '
+    ENV["JULIA_DEBUG"] = "all";
     import Pkg;
     Pkg.add("PredictMD");
     Pkg.build("PredictMD");
     '
 
 julia $JULIA_FLAGS -e '
+    ENV["JULIA_DEBUG"] = "all";
     import Pkg;
     Pkg.add("PredictMDExtra");
     Pkg.build("PredictMDExtra");
     '
 
-# julia $JULIA_FLAGS -e '
-#     import Pkg;
-#     Pkg.add("PredictMDFull");
-#     Pkg.build("PredictMDFull");
-#     '
+julia $JULIA_FLAGS -e '
+    ENV["JULIA_DEBUG"] = "all";
+    import Pkg;
+    Pkg.add("PredictMDFull");
+    Pkg.build("PredictMDFull");
+    '
 
 julia $JULIA_FLAGS -e '
+    ENV["JULIA_DEBUG"] = "all";
     import Pkg;
     Pkg.build("PredictMD");
     Pkg.build("PredictMDExtra");
+    Pkg.build("PredictMDFull");
     '
-# julia $JULIA_FLAGS -e '
-#     import Pkg;
-#     Pkg.build("PredictMD");
-#     Pkg.build("PredictMDExtra");
-#     Pkg.build("PredictMDFull");
-#     '
 
 ##### End of file
